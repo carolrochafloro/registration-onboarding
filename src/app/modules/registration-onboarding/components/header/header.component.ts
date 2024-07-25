df4d9f1c95ece518b7413182d9136c8062acefb5
Carolina Floro <carolinarochaf@hotmail.com>
2024-07-25 17:33:19 -0300

    Set input to display current step and stage in the header

--- src/app/modules/registration-onboarding/components/header/header.component.ts
+++ src/app/modules/registration-onboarding/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() step: number | undefined;
+  @Input() stage: string | undefined;
+}
--- src/app/modules/registration-onboarding/pages/individual/individual.component.ts
+++ src/app/modules/registration-onboarding/pages/individual/individual.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-individual',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './individual.component.html',
-  styleUrl: './individual.component.scss'
+  styleUrl: './individual.component.scss',
 })
 export class IndividualComponent {
-
+  step = 1;
+  stage = 'Personal Info.';
 }
