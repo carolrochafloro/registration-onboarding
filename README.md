2cd3c2d735e3a89b09eca3068e751ff1382887fe
Carolina Floro <carolinarochaf@hotmail.com>
2024-07-26 10:48:52 -0300

    Implemented template driven form on individual component

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Using a public template available on figma to develop using Angular 17.
 
 Figma: [template](https://www.figma.com/design/40tenCelTiMi2CzeY0iop5/Registration-Onboarding-Design?node-id=1-649)
+
+## Form model
+
+For this project I chose to implement template driven forms since there's no need of complex logic or validations whatsoever.
--- src/app/modules/registration-onboarding/pages/individual/individual.component.ts
+++ src/app/modules/registration-onboarding/pages/individual/individual.component.ts
@@ -1,14 +1,40 @@
 import { Component, Output } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { NgForm, FormsModule } from '@angular/forms';
+import { IUser } from '../../interfaces/individual.interface';
 
 @Component({
   selector: 'app-individual',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormsModule],
   templateUrl: './individual.component.html',
   styleUrl: './individual.component.scss',
 })
 export class IndividualComponent {
   step = 1;
   stage = 'Personal Info.';
+
+  fullName = '';
+  email = '';
+  password = '';
+  agreeTerms = false;
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      const user: IUser = {
+        fullName: form.value.fullName,
+        email: form.value.email,
+        password: form.value.password,
+      };
+
+      localStorage.setItem('user', JSON.stringify(user));
+
+      form.resetForm({
+        fullName: '',
+        email: '',
+        password: '',
+        agreeTerms: false,
+      });
+    }
+  }
 }
