8ee1ac2d735cd3af9ce7dc01c85f776fa9af7d5a
Carolina Floro <carolinarochaf@hotmail.com>
2024-07-29 09:43:25 -0300

    Created step 2 component

--- src/app/modules/registration-onboarding/components/individual-step2/individual-step2.component.ts
+++ src/app/modules/registration-onboarding/components/individual-step2/individual-step2.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IUser } from '../../interfaces/individual.interface';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-individual-step2',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [FormsModule],
   templateUrl: './individual-step2.component.html',
   styleUrl: './individual-step2.component.scss',
 })
 export class IndividualStep2Component {
-  step = 2;
-  stage = 'Residency Info.';
+  @Input() user?: IUser;
+
+  country = ['Brazil', 'Morocco', 'Poland', 'France'];
+  selectedCountry = '';
+
+  onCountryChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedCountry = selectElement.value;
+  }
+
+  @Output() formSubmitted = new EventEmitter<IUser>();
+
+  onSubmit(form: NgForm) {
+    if (this.user) {
+      this.user.residencyInfo = {
+        phone: form.value.phone,
+        address: form.value.address,
+        country: this.selectedCountry,
+      };
+
+      this.formSubmitted.emit(this.user);
+      form.resetForm();
+    }
+  }
 }
