a9d34b1dae896f0da7bfa871b7e9f6e2d7622c19
Carolina Floro <carolinarochaf@hotmail.com>
2024-07-29 09:45:16 -0300

    Created join page and a new route for individual accounts

--- src/app/modules/registration-onboarding/pages/join/join.component.ts
+++ src/app/modules/registration-onboarding/pages/join/join.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { JoinUsComponent } from '../../components/join-us/join-us.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-join',
   standalone: true,
-  imports: [RouterModule],
+  imports: [JoinUsComponent],
   templateUrl: './join.component.html',
   styleUrl: './join.component.scss',
 })
-export class JoinComponent {}
+export class JoinComponent {
+  constructor(private router: Router) {}
+
+  handleButtonClick(accountType: string) {
+    switch (accountType) {
+      case 'individual':
+        this.router.navigate(['/individual']);
+        break;
+      case 'business':
+        this.router.navigate(['/business']);
+        break;
+      default:
+        console.error('Unknown account type:', accountType);
+        break;
+    }
+  }
+}
